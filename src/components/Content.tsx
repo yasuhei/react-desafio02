e03c5b14c774cbcefba53f00635c0b8070c88053
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-03-21 20:26:23 -0300

    Desafio 02 ignit completo!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,44 @@
-export function Content() {
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'document' | 'drama'| 'horror'| 'family'; 
+  title: string
+};
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+};
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps
+  movies: Array<MovieProps>
+};
+export function Content({ selectedGenre, movies}: ContentProps) {
   // Complete aqui
+
+  return(
+    <div className='container'>
+      <header>
+        <span className='categoria'>Categoria:<span>{selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className='movies-list'>
+          {movies.map((movies: MovieProps) => (
+            <MovieCard key={movies.Title} title={movies.Title} poster={movies.Poster} runtime={movies.Runtime} rating={movies.Ratings[0].Value} />
+          )) }
+
+        </div>
+      </main>
+
+    </div>
+  )
 }
